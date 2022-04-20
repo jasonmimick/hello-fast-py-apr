e04038f84fb3e43d510b56a7b71b0d9edba30357
Jason Mimick <jmimick@gmail.com>
2022-04-20 11:11:31 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+import requests
 import uvicorn
 
 app = FastAPI()
@@ -8,6 +9,9 @@ app = FastAPI()
 async def root():
     return {"message": "Hello World"}
 
+@app.get("/population")
+async def population():
+    return requests.get("https://datausa.io/api/data\?drilldowns\=Nation\&measures\=Population").json()
 
 if __name__ == '__main__':
     uvicorn.run(app, port=8080, host='0.0.0.0')
